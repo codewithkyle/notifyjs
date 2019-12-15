d7ab6ffe33426824d2e257fa730fd260a8aef425
Kyle Andrews <kylea@page.works>
2019-12-15 15:11:24 -0500

    Adds TypeScript declarations file

--- src/Notify.ts
+++ src/Notify.ts
@@ -1,3 +1,27 @@
+interface NotificationButton
+{
+    label: string,
+    callback: Function,
+    ariaLabel?: string,
+}
+
+interface SnackbarNotification
+{
+    message: string,
+    duration?: number,
+    closeable?: boolean,
+    buttons?: Array<NotificationButton>,
+    position?: string,
+    element?: HTMLElement,
+    force?: boolean,
+}
+
+interface VerificationResponse
+{
+    validNotification: SnackbarNotification,
+    warnings: Array<string>,
+}
+
 export class NotificationManager
 {
     private _queue: Array<SnackbarNotification>;
