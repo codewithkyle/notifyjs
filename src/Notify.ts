8847d1403d97c97aae9dd6709c82b587a2d0bb49
Kyle Andrews <kylea@page.works>
2020-02-11 08:24:44 -0500

    Adds test

--- src/Notify.ts
+++ src/Notify.ts
@@ -1,19 +1,19 @@
-import { NotificationManager } from './notification-manager';
+import { NotificationManager } from "./notification-manager";
 
 const globalManager = new NotificationManager();
 
-const notify:(notification:{
-    message: string,
-    duration?: number,
-    closeable?: boolean,
+const notify: (notification: {
+    message: string;
+    duration?: number;
+    closeable?: boolean;
     buttons?: Array<{
-        label: string,
-        callback: Function,
-        ariaLabel?: string,
-        classes?: Array<string>|string,
-    }>,
-    force?: boolean,
-    classes?: Array<string>|string,
-})=>void = globalManager.notify.bind(globalManager);
+        label: string;
+        callback: Function;
+        ariaLabel?: string;
+        classes?: Array<string> | string;
+    }>;
+    force?: boolean;
+    classes?: Array<string> | string;
+}) => void = globalManager.notify.bind(globalManager);
 
 export { NotificationManager, notify };
