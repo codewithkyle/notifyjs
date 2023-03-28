d0ed6db5fe8a216a9e56c5ff03aa99be3df625d1
Kyle Andrews <codingwithkyle@gmail.com>
2023-03-28 15:01:16 -0400

    ✨ Adding demo deployment script

---
+++ build/deploy.js
@@ -0,0 +1,23 @@
+import ghPages from "gh-pages";
+
+const NAME = "Kyle Andrews";
+const EMAIL = "codingwithkyle@gmail.com";
+const USERNAME = "codewithkyle";
+const PROJECT = "notifyjs";
+
+ghPages.publish(
+    "public",
+    {
+        user: {
+            name: NAME,
+            email: EMAIL,
+        },
+        repo: "https://" + process.env.ACCESS_TOKEN + "@github.com/" + USERNAME + "/" + PROJECT + ".git",
+        silent: true,
+    },
+    (error) => {
+        if (error) {
+            console.log(error);
+        }
+    }
+);
